7a5edd43f1e2c88e7a852ca6e11f73e85f18a489
dmitriy <machortovdima@mail.ru>
2023-03-06 19:20:37 +0300

    added task2 home_work1

---
+++ home_work1/task2.java
@@ -0,0 +1,24 @@
+package Home_work_Java.home_work1;
+
+public class task2 {
+    boolean is_simple(int n) {
+        for (int index = 2; index < n; index++) {
+            if (n % index == 0 )
+                return false;
+        }
+        return true;
+
+    }
+    public static void main(String[] args) {
+        task2 t = new task2();
+        StringBuilder st = new StringBuilder();
+        st = st.append("1");
+        for (int i = 2; i < 1000; i++) {
+            if (t.is_simple(i))
+                st = st.append(", ").append(i);
+        }
+    System.out.println("Простые числа от 1 до 1000: ");   
+    System.out.println(st);   
+    }
+}
+
